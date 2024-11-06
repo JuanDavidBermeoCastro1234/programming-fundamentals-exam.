1bf9853da14fead8f6181e3fa2d4025f16a09d42
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-06 11:29:52 -0500

    solution ejercise 4

--- mayn.py
+++ mayn.py
@@ -1,12 +1,4 @@
-voltaje=float(input("ingrese un voltaje por favor: "))
-voltaje2=float(input("ingrese un voltaje distinto al primero por favor: "))
-voltaje3=float(input("ingrese un voltaje distinto a los otros dos por favor: "))
+metros=float(input("por favor ingrese los metros que desea convertir a kilometros "))
 
-promedio=voltaje+voltaje2+voltaje3/3
-
-if promedio<115:
-    print(f"VOLTAJE CORRECTO {promedio:.2f}")
-elif promedio>115<220:
-    print(f"ALTO VOLTAJE {promedio:.2f}")
-elif promedio>220:
-    print(f"PELIGRO {promedio:.2f}")
+kilometros=metros/1000
+print(f"los metros ingresados {metros} sonh en kilometros {kilometros}")
