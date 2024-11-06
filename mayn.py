a070df3555f0b28fd68fe073966691a5cb287dc1
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-06 11:26:55 -0500

    solution ejercise 1

--- mayn.py
+++ mayn.py
@@ -3,10 +3,10 @@ voltaje2=float(input("por favor ingrese el voltaje: "))
 voltaje3=float(input("por favor ingrese el voltaje: "))
 voltaje4=float(input("por favor ingrese el voltaje: "))
 voltaje5=float(input("por favor ingrese el voltaje: "))
-
+#
 promedio= voltaje+voltaje2+voltaje3+voltaje4 /5
 
 if promedio<220:
     print(f"voltaje {promedio:.2f} correcto ")
 else :
-    print("alto voltaje ") 
+    print("alto voltaje ")
